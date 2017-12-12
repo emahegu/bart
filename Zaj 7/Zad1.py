20b32387462c0a6753407bcaa7015c8978eabaf2
emahegu <emahegu@yopmail.com>
2017-12-12 15:11:49 +0100

    sdf

---
+++ Zaj 7/Zad1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 18:17:58 2017
+
+@author: biegbart
+"""
+function pierwsza(liczba):
+    for i in range(1,liczba)
+        if ()    
+    
+    
+    
+
+lista = [5,7,4,5,6,12]
+wyn=[]
+mx = 0
+for i in lista:
+    if (i>mx):
+        mx=i
+for i in range(0,mx):
+    wyn.append(0)
+    
+for i in lista:
+    wyn[i]+=1
+
+
+
